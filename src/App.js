49b407844ee29c4a8ae0d7f316fa5bed8d99fd5d
Hashir.a <hashir.a@outlook.com>
2020-08-24 12:21:55 +0500

    updated include name

--- src/App.js
+++ src/App.js
@@ -10,12 +10,15 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <h2>Made By Hashir</h2>
         <a
           className="App-link"
           href="https://reactjs.org"
+      
           target="_blank"
           rel="noopener noreferrer"
         >
+         
           Learn React
         </a>
       </header>
